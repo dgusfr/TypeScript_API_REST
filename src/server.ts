b4a4ae1ad6ce50877c57cbce14dd696762feaad1
dgusfr <diegogustavo.fr@gmail.com>
2024-12-24 08:43:07 -0300

    feat: incluir middleware de log no server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express, { Application } from 'express';
 import { router } from './routes';
+import { logMiddleware } from './middlewares/logMiddleware';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(logMiddleware);app.use(logMiddleware);
 app.use(express.json());
 app.use('/api', router);
 
